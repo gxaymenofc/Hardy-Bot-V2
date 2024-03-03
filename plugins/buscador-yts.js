3f3625c979b69f374abdf92f1bb9d058816e0f1a
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-03-03 10:01:43 +0100

    unzipgit

---
+++ plugins/buscador-yts.js
@@ -0,0 +1,23 @@
+import yts from 'yt-search';
+import fs from 'fs';
+
+const handler = async (m, {conn, text}) => {
+  if (!text) throw '⚠️ *_ما هو عنوان الفيديو الذي تبحث عنه?_*';
+  const results = await yts(text);
+  const tes = results.all;
+  const teks = results.all.map((v) => {
+    switch (v.type) {
+      case 'video': return `
+° *_${v.title}_*
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_المدة :_* ${v.timestamp}
+↳ 📥 *_تاريخ التنزيل :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`;
+    }
+  }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
+};
+handler.help = ['ytsearch *<texto>*'];
+handler.tags = ['search'];
+handler.command = ['ytsearch', 'yts'];
+export default handler;
